6ee88ab3b3d0bd5b70fa42fd7f67b3182f7cf3a3
soumnsdt <30397118+soumnsdt@users.noreply.github.com>
2019-02-12 23:36:22 +0800

    led闪烁
    led闪烁

--- main.c
+++ main.c
@@ -1,12 +1,24 @@
 #include<reg52.h>
 
-sbit led = P1^0; //位定义
+typedef unsigned char u8;
+typedef unsigned int u16;
+sbit led = P0^0; //位定义
+
+
+void delay1ms(u16 num)   //大约延时0.9ms
+{
+	u16 x,y;
+	for(x = num;x > 0;x--)
+		for(y = 110;y > 0;y--);
+}
 
 int main()
 {
-	led = 1;
 	while(1)
 	{
-		led = 0;   //让led灯熄灭   因为led灯一开始就是亮的
+		led = 0;
+		delay1ms(5000);//大约延时4.4s
+		led = 1;
+		delay1ms(5000);
 	}
 }
